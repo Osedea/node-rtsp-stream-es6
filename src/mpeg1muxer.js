c1d88985cf1f436c9727c915d299719f94ec2c1f
Evan <evang9410@gmail.com>
2018-02-05 16:39:26 -0500

    rtsp transport coversion changes

--- src/mpeg1muxer.js
+++ src/mpeg1muxer.js
@@ -1,21 +1,20 @@
-const child_process = require('child_process')
-const EventEmitter = require('events')
+const child_process = require('child_process');
+const EventEmitter = require('events');
 
 class Mpeg1Muxer extends EventEmitter {
+    constructor(options) {
+        super(options);
+        this.url = options.url;
+        this.stream = child_process.spawn(
+            'ffmpeg',
+            ['-loglevel', 'quiet', '-rtsp_transport', 'tcp', '-i', this.url, '-f', 'mpegts', '-codec:v', 'mpeg1video', '-b:v', '180k', '-r', '30', '-'],
+            { detached: false }
+        );
 
-  constructor(options) {
-    super(options)
-    
-    this.url = options.url
-    
-    this.stream = child_process.spawn("ffmpeg", ["-rtsp_transport", "tcp", "-i", this.url, '-f', 'mpeg1video', '-b:v', '180k', '-r', '30', '-'], {
-      detached: false
-    })
-    
-    this.inputStreamStarted = true
-    this.stream.stdout.on('data', (data) => { return this.emit('mpeg1data', data) })
-    this.stream.stderr.on('data', (data) => { return this.emit('ffmpegError', data) })
-  }
+        this.inputStreamStarted = true
+        this.stream.stdout.on('data', (data) => { return this.emit('mpeg1data', data) })
+        this.stream.stderr.on('data', (data) => { return this.emit('ffmpegError', data) })
+    }
 }
 
-module.exports = Mpeg1Muxer
+module.exports = Mpeg1Muxer;
