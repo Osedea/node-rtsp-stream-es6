eabe9e10c9dd3c73f906f001a3e35022f57a4c7f
Evan <evang9410@gmail.com>
2018-03-20 18:11:48 -0400

    trying out some stuff

--- src/mpeg1muxer.js
+++ src/mpeg1muxer.js
@@ -7,8 +7,11 @@ class Mpeg1Muxer extends EventEmitter {
         this.url = options.url;
         this.stream = child_process.spawn(
             'ffmpeg',
+            // -vf "crop=iw-mod(iw\,2):ih-mod(ih\,2)" -b 0
+            // '-bufsize', '512k', '-maxrate', '3000k'
             // ['-rtsp_transport', 'tcp','-i', this.url, '-vcodec', 'copy', '-probesize', '200000', '-an', '-f', 'mp4', '-reset_timestamps', '1', '-movflags', 'empty_moov+default_base_moof+frag_keyframe', '-loglevel','debug', '-'],
-            ['-loglevel', 'quiet', '-rtsp_transport', 'tcp', '-i', this.url, '-f', 'mpegts', '-codec:v', 'mpeg1video', '-bf', '800k', '-r', '30', '-'],
+            ['-loglevel', 'quiet', '-rtsp_transport', 'tcp', '-re', '-i', this.url, '-s','1280x720','-an', '-f', 'mpegts','-codec:v', 'mpeg1video','-g', '30', '-bf', '0', '-b:v', '512k','-q', '20', '-'],
+            // ['-loglevel', 'debug', '-rtsp_transport', 'tcp', '-i', this.url, '-f', 'mpegts', '-vf', 'crop=iw-mod(iw\\,2):ih-mod(ih\\,2)', '-b', '0', '-' ],
             { detached: false }
         );
 
