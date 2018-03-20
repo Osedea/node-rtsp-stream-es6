c0fd75c687780faf26ed1a640d0133a012fedfc3
Evan <evang9410@gmail.com>
2018-03-20 18:27:58 -0400

    as fluent as it's getting

--- src/mpeg1muxer.js
+++ src/mpeg1muxer.js
@@ -10,8 +10,10 @@ class Mpeg1Muxer extends EventEmitter {
             // -vf "crop=iw-mod(iw\,2):ih-mod(ih\,2)" -b 0
             // '-bufsize', '512k', '-maxrate', '3000k'
             // ['-rtsp_transport', 'tcp','-i', this.url, '-vcodec', 'copy', '-probesize', '200000', '-an', '-f', 'mp4', '-reset_timestamps', '1', '-movflags', 'empty_moov+default_base_moof+frag_keyframe', '-loglevel','debug', '-'],
-            ['-loglevel', 'quiet', '-rtsp_transport', 'tcp', '-re', '-i', this.url, '-s','1280x720','-an', '-f', 'mpegts','-codec:v', 'mpeg1video','-g', '30', '-bf', '0', '-b:v', '512k','-q', '20', '-'],
+            // -f mpegts -codec:v mpeg1video -s 960x540 -b:v 1500k -r 30 -bf 0
+            // ['-loglevel', 'quiet', '-rtsp_transport', 'tcp', '-re', '-i', this.url, '-s','1280x720','-an', '-f', 'mpegts','-codec:v', 'mpeg1video','-g', '30', '-bf', '0', '-b:v', '512k','-q', '20', '-'],
             // ['-loglevel', 'debug', '-rtsp_transport', 'tcp', '-i', this.url, '-f', 'mpegts', '-vf', 'crop=iw-mod(iw\\,2):ih-mod(ih\\,2)', '-b', '0', '-' ],
+            ['-loglevel', 'debug', '-rtsp_transport', 'tcp', '-re', '-i', this.url, '-s', '1280x720', '-an', '-f', 'mpegts', '-codec:v', 'mpeg1video', '-b:v', '1500k', '-r', '30', '-bf', '10', '-q', '20', '-'],
             { detached: false }
         );
 
